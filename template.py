34813a7617b99e1d7f45560c1a34b393d0febdb3
Admin <rama.sahoo@gmail.com>
2025-08-30 08:58:03 +0530

    2nd commit

--- template.py
+++ template.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s ]: %(message)s:')
-
 project_name = "datascience"
 
 list_of_files = [
@@ -37,12 +36,9 @@ for filepath in list_of_files:
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory {filedir} for the file : {filename}")
-
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
-
             pass
             logging.info(f"Creating empty file: {filepath}")
-
     else:
         logging.info(f"{filename} is already exists")
